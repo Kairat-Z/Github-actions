5f653941b530f3888b7fd60668749fa6e9f55c8a
kairat-z <zh.kairat2@gmail.com>
2025-02-01 19:55:37 +0000

    123333

---
+++ main.tf
@@ -0,0 +1,24 @@
+provider aws {
+    region = "us-east-2"
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
